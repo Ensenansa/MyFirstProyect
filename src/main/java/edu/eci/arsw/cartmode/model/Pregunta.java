b5d3adcc603d4b3f905bf443f095fa2ef8937f18
cesareduardo824 <cesareduardo824@gmail.com>
2019-02-24 03:04:23 -0500

    Comenzando con clases

---
+++ src/main/java/edu/eci/arsw/cartmode/model/Pregunta.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.cartmode.model;
+
+/**
+ *
+ * @author cesar
+ */
+public interface Pregunta {
+    
+}
