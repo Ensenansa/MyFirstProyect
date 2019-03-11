98bd09b59611453a655cde92e502904d4c406a74
cesareduardo824 <cesareduardo824@gmail.com>
2019-03-11 08:25:22 -0500

    Creacion REST para cartas

---
+++ src/main/java/edu/eci/arsw/cartmode/controllers/CartModeCOntrolerCarts.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.cartmode.controllers;
+
+import edu.eci.arsw.cartmode.services.CartModeException;
+import edu.eci.arsw.cartmode.services.CartModeServices;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author cesar
+ */
+@RestController
+@RequestMapping(value = "/jugadores")
+public class CartModeCOntrolerCarts {
+    
+    @Autowired
+    private CartModeServices cat;
+    
+    
+    @RequestMapping(method = RequestMethod.GET, path = "/sala/{nombre}")
+    public ResponseEntity<?> getAllPlayerByLevel(@PathVariable Integer nivel) {
+        try {
+            //cambiar
+            return new ResponseEntity<>(cat.getPlayerAnfiBySala(nivel) , HttpStatus.ACCEPTED);
+        } catch (CartModeException ex) {
+            Logger.getLogger(CartModeController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error", HttpStatus.NOT_FOUND);
+        }
+    }
+    
+    
+    
+}
